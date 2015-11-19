f4967fdcad71078f7c1b28cec07facd11580094c
jerry <jerry@polaris.local>
2015-11-19 15:39:58 +0800

    Changes to be committed: 	new file:   subprocess_exe.py  Changes not staged for commit: 	deleted:    subprocess-exe.py

---
+++ subprocess_exe.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'jerry'
+__version__= 'v1'
+__copyright__ = '2014@jerry.D'
+
+"""
+@since: 2014-07-20
+@summary: 
+    execute shell script command via creating subprocess.
+    python script filter STDOUT text,and split key-word that u provided to get return value.
+"""
+
+import subprocess
+import sys
+
+__BUFSIZE__ = 1024
+
+# stdout without return value
+# cmdArgsList : shell name and cmd args
+#               cmdArgsList=['ping','google.com','-c','10']
+def subprocess_stdout(cmdArgsList):
+    try:
+        sp = subprocess.Popen(cmdArgsList,bufsize=__BUFSIZE__,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
+        spid = sp.pid
+        while True:
+            nextLine = sp.stdout.readline()
+            if( nextLine == '' and sp.poll() != None):
+                break
+            sys.stdout.write(nextLine)
+    except Exception, e:
+        print e
+
+# return value via stdout PIPE filter.
+# cmdArgsList : shell name and cmd args
+#               cmdArgsList=['ping','google.com','-c','10']
+# rvKeyword : set rvKeyword=key, 'subprocess_returnvalue' will filter text line like 'key=value',
+#             when 'key=value' appeared, subprocess will be terminated, and 'value' be returned.
+def subprocess_returnvalue(cmdArgsList,rvKeyword):
+    try:
+        sp = subprocess.Popen(cmdArgsList,bufsize=__BUFSIZE__,stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=False)
+        spid = sp.pid
+        while True:
+            nextLine = sp.stdout.readline()
+            if( nextLine == '' and sp.poll() != None):
+                break
+            resultArray = nextLine.split('%s='%rvKeyword)
+            if len(resultArray) == 2:
+                sp.terminate()
+                return resultArray[1]
+    except Exception, e:
+        print e
+
+if __name__ == '__main__':
+    cmdList=['ping','baidu.com','-c','100']
+    print subprocess_returnvalue(cmdList,'time')
